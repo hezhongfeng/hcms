5b1c67f3e9056ca8a286a27db2d17b75af3219c4
hezhongfeng <287645587@qq.com>
2020-10-10 14:44:13 +0800

    feat(auth): 完成基础的认证授权

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly authService: AuthService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // 内置的login拦截处理
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { modules, dbConfig } from './app.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [...modules, TypeOrmModule.forRoot(dbConfig)],
+  imports: [...modules, TypeOrmModule.forRoot(dbConfig), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
